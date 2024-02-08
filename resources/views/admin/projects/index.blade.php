8b357e2751b7088b80a7f7480977195dcf46ab3e
OgataiKhan <badellino.kevin@gmail.com>
2024-02-08 06:25:04 +0100

    feat: data table display

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.admin')
 
 @section('content')
-    <ul class="py-4">
-      @foreach ($projects as $project)
-          <li>
-            {{ $project->title }}
-          </li>
-      @endforeach
-    </ul>
+    <div class="container pt-4">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">title</th>
+                    <th scope="col">slug</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{ $project->id }}</td>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->slug }}</td>
+                        <td>
+                          <a rel="stylesheet" href="" role="button" class="btn btn-info btn-sm">show</a>
+                          <a rel="stylesheet" href="" role="button" class="btn btn-primary btn-sm">edit</a>
+                          <a rel="stylesheet" href="" role="button" class="btn btn-danger btn-sm">delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
